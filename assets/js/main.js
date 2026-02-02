4a55faa27e35fa377fd683382a7d00647d31591c
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-02 14:33:47 +0100

    working on the final step

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,30 @@
 - Conditionals (if, else) */
 
 /* VARIABLES */
-/* Utilizzo parseInt per l'età perché è sempre un numero tondo mentre invece parseFloat per i KM perché potrebbero avere decimali */
+/* I'm using parseInt for age because it's always a whole number, whereas I use parseFloat for kilometers because they can have decimals */
 
 const kilometers = parseFloat(prompt('How many kilometers do you want to travel?'))
 const age = parseInt(prompt('How old are you?'))
-
+const price = kilometers * 0.21
 
 /* TESTING IF IT WORKS */
+
 console.log(kilometers, age)
+
+
+/* IF/ELSE */
+/* IF age is less than 18, apply a 20% discount  */
+/* ELSE IF age is greater than 65, apply a 40% discount  */
+/* ELSE age is a number between 19 and 65, do nothing*/
+
+if (age < 18) {
+    price = price - (price * 20) / 100;
+} else if (age > 65) {
+    price = price - (price * 40) / 100;
+} else {
+
+}
+
+/* PRINTING FINAL PRICE FOR THE USER */
+
+console.log("The price of your ticket is:" + price)
